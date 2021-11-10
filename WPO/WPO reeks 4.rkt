3bcbf0c695bdf516968069ecf6b14681bb6df809
Vic De Hondt <vicdehondt@Vics-MacBook-Pro.local>
2021-11-10 17:49:55 +0100

    reeks 4 verbeterd

--- WPO/WPO reeks 4.rkt
+++ WPO/WPO reeks 4.rkt
@@ -1,21 +1,16 @@
+; 4.7
 (define (sum term a next b)
   (if (> a b)
       0
       (+ (term a) (sum term (next a) next b))))
 
+; 4.7.1
 (define (product factor a next b)
   (if (> a b)
       1
       (* (factor a) (product factor (next a) next b))))
 
-;(define (iter-product factor a next b)
-;  (define (iter factor a next b count result)
-;    (if (< count 0)
-;        result
-;        (iter factor (next a) next b (- count 1) (* (factor a) result))))
-;  (iter factor a next b (- b a) 1))
-
-; assistent:
+; 4.7.2
 (define (iter-product factor a next b)
   (define (iter i result)
     (if (> i b)
@@ -25,9 +20,11 @@
 
 (define (id x) x)
 
+; 4.7.3
 (define (factorial n)
   (product id 1 (lambda (x) (+ x 1)) n))
 
+; 4.9.1
 (define (accumulate combiner null-value term a next b)
   (define (iter i result)
   (if (<= i b)
@@ -35,18 +32,21 @@
       result))
   (iter a null-value))
 
+; 4.9.2
 (define (sum term a next b)
   (accumulate + 0 term a next b))
 
 (define (product factor a next b)
   (accumulate * 1 factor a next b))
 
+; 4.9.3
 (define (add a b)
   (accumulate + a (lambda (i) 1) 1 (lambda (i) (+ i 1)) b))
 
 (define (multiply a b)
   (accumulate + 0 (lambda (x) a) 1 (lambda (x) (+ x 1)) b))
 
+; 4.10.1
 (define (filtered-accumulate combiner filter? null-value term a next b)
   (define (iter a res)
     (if (<= a b)
@@ -56,6 +56,15 @@
         res))
   (iter a null-value))
 
+; 4.10.2
+(define (product-gcd n)
+  (define (incr x) (+ x 1))
+  (define (id x) x)
+  (define (filter? a)
+    (= (gcd a n) 1))
+  (filtered-accumulate * filter? 1 id 1 incr n))
+
+; 4.14
 (define ((compose f g) x)
   (f (g x)))
 
@@ -64,6 +73,7 @@
 ;  (lambda (x)
 ;    (f (g x))))
 
+; 4.15
 (define (do-n f n)
   (define (iter counter)
     (if (= counter n)
